64b2322619c93b40af2f4c0b01a60c23902fe49f
Nazmul Hossain <nazmulhossain2905@gmail.com>
2026-09-10 22:00:44 +0600

    chore(initialization): project initialization and setup tailwindcss, react-icons, react-tostify, and folder structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ToastContainer } from "react-toastify";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      {/* For React Toastify */}
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
